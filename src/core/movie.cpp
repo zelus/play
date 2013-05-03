4fcd7b58ea66f0f82cb426d2a51f1e5d80993ba4
Gwendal-netbook <zelus44@gmail.com>
2013-05-03 17:23:52 +0200

    add second Movie constructor allowing notation and summary setting

--- src/core/movie.cpp
+++ src/core/movie.cpp
@@ -8,13 +8,28 @@
   summary are setted to default values (respectively 0 and "").
   \param movieName the name of the Movie.
   \param parent the parent of the Movie
-  \todo An other constructor should be defined that allow a Movie creation with
-  a given notation and a given summary.
  */
 Movie::Movie(const string& movieName, Item* parent) : Item(movieName,MOVIE_TYPE,parent)
 {
     notation_ = 0;
     summary_ = "";
+}
+
+/*!
+  \brief Constructs a Movie from the given parameters.
+
+  The given name and parent are delegated to the Item constructo, and
+  the ItemType is statically defined as MOVIE_TYPE. Notation and summary
+  are setted with the given values.
+  \param movieName the name of the Movie.
+  \param movieSummary the summary of the Movie.
+  \param movieNotation the notation of the Movie.
+  \param parent the parent of the Movie.
+ */
+Movie::Movie(const string &movieName, const string &movieSummary, const short movieNotation, Item *parent) : Item(movieName, MOVIE_TYPE,parent)
+{
+    notation_ = movieNotation;
+    summary_ = movieSummary;
 }
 
 /*!
