8cc7fa6783f3aed4123e68ed89861d20e243844f
Gwendal-netbook <zelus44@gmail.com>
2013-05-03 11:13:48 +0200

    add Movie documentation

--- src/core/movie.cpp
+++ src/core/movie.cpp
@@ -1,37 +1,72 @@
 #include "movie.h"
 
+/*!
+  \brief Constructs a Movie from the given parameters.
+
+  The given name and parent are delegated to the Item constructor, and
+  the ItemType is statically defined as MOVIE_TYPE. The notation and the
+  summary are setted to default values (respectively 0 and "").
+  \param movieName the name of the Movie.
+  \param parent the parent of the Movie
+  \todo An other constructor should be defined that allow a Movie creation with
+  a given notation and a given summary.
+ */
 Movie::Movie(const string& movieName, Item* parent) : Item(movieName,MOVIE_TYPE,parent)
 {
     notation_ = 0;
     summary_ = "";
 }
 
+/*!
+  \brief Constructs a Movie from the given Movie.
+
+  Notation and summary of the given movie are also copied.
+  \param movie the Movie to construct from.
+ */
 Movie::Movie(const Movie &movie) : Item(movie.itemName_,MOVIE_TYPE,movie.parent_)
 {
     notation_ = movie.notation_;
     summary_ = movie.summary_;
 }
 
+/*!
+  \brief Deletes the Movie.
+  \note Parent management is handled in Item superclass.
+ */
 Movie::~Movie()
 {
 
 }
 
+/*!
+  \return the summary of the Movie.
+ */
 const string& Movie::getSummary() const
 {
     return summary_;
 }
 
+/*!
+  \return the notation of the Movie.
+ */
 short Movie::getNotation() const
 {
     return notation_;
 }
 
+/*!
+  \brief Sets a new summary to the Movie.
+  \param summary the new summary.
+ */
 void Movie::setSummary(const string &summary)
 {
     summary_ = summary;
 }
 
+/*!
+  \brief Sets a new notation to the Movie.
+  \param notation the new notation.
+ */
 void Movie::setNotation(const short notation)
 {
     notation_ = notation;
--- src/core/movie.h
+++ src/core/movie.h
@@ -1,9 +1,24 @@
+/*!
+  \file movie.h
+  \author Gwendal Daniel
+  \date 2013-04-28
+ */
+
 #ifndef MOVIE_H
 #define MOVIE_H
 
 #include "common.h"
 #include "item.h"
 
+/*!
+  \brief Represents a concrete Item with movies informations
+
+  The Movie class is a part of the \em composite pattern used to store
+  the Item tree. It is derivated from the Item class and represents a movie
+  with specific movie informations : summary and notation.
+  \note Movie class doesn't override children management method because a Movie
+  is a basic Item and can not have children.
+ */
 class Movie : public Item
 {
 public:
