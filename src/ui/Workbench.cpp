a2efc1705b65deb2fa642d5de04c6867e9d0969e
gwendal <gwendal.daniel@gmail.com>
2013-09-04 15:53:24 +0200

    add workbench class

--- src/ui/Workbench.cpp
+++ src/ui/Workbench.cpp
@@ -1,6 +1,35 @@
 #include "Workbench.h"
 
+#include <QTextEdit>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
+#include <QHBoxLayout>
+#include <QSplitter>
+
 Workbench::Workbench(QWidget *parent) : QMainWindow(parent)
 {
     setWindowTitle("play");
+
+    QWidget* central = new QWidget(this);
+    QHBoxLayout* central_layout = new QHBoxLayout;
+
+    QTreeWidget* libTree = new QTreeWidget;
+    libTree->setColumnCount(1);
+    libTree->setHeaderLabel("name");
+    QTreeWidgetItem* lib1 = new QTreeWidgetItem(QStringList("Lib1"));
+    lib1->addChild(new QTreeWidgetItem(QStringList("mov1")));
+    QList<QTreeWidgetItem*> topLevelItems;
+    topLevelItems.append(lib1);
+    topLevelItems.append(new QTreeWidgetItem(QStringList("B Lib")));
+    topLevelItems.append(new QTreeWidgetItem(QStringList("A Lib")));
+    libTree->insertTopLevelItems(0,topLevelItems);
+    libTree->setSortingEnabled(true);
+    QTextEdit* edit2 = new QTextEdit("main view");
+
+    central_layout->addWidget(libTree);
+    central_layout->addWidget(edit2);
+
+    central->setLayout(central_layout);
+
+    setCentralWidget(central);
 }
